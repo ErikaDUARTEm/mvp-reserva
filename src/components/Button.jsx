f62549fb5744b90fd8ec746ac47c62a40e7c260a
Erika Duarte <erika.duarte.mora@gmail.com>
2024-04-09 14:18:29 -0500

    Updates

--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-export function Button ({children}){
-    return(
-        <>
-            <button type="button" className="btn"> {children} </button>
-        </>
-    )
-}
--- src/config-router/paths.js
+++
@@ -1,3 +0,0 @@
-export const HOME = "/mvp-reserva/";
-export const HOURS = "/mvp-reserva/turnos";
-export const CONFIRM = "/mvp-reserva/turnos/confirmacion";
